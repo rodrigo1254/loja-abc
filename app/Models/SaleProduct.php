2153b73cf4f1b50f9acea0a79048224943390cee
Rodrigo Campos <rodrigo1254@gmail.com>
2024-03-01 11:35:46 -0300

    arquivos de migrate

---
+++ app/Models/SaleProduct.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class SaleProduct extends Model
+{
+    use HasFactory,SoftDeletes;
+
+    //protected $fillable = ['name,','price','description'];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class); //tem mts produtos
+    }
+}
