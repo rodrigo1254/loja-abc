f05102768370251d331c83a3a1c90f08e9b935b2
Rodrigo Campos <rodrigo1254@gmail.com>
2024-03-04 09:09:31 -0300

    anexado ./docker

--- routes/api.php
+++ routes/api.php
@@ -11,16 +11,6 @@ Route::apiResource('sales','App\Http\Controllers\Api\V1\SalesController');
 Route::put('sales/{id}/cancel', ['App\Http\Controllers\Api\V1\SalesController', 'cancel']);
 Route::post('sales/{id}', ['App\Http\Controllers\Api\V1\SalesController', 'addProductsToSale']);
 
-/*
-Route::get('sales',['App\Http\Controllers\Api\V1\SalesController','index']);
-Route::get('sales/{sale}',['App\Http\Controllers\Api\V1\SalesController','show']);
-
-Route::post('sales',['App\Http\Controllers\Api\V1\SalesController','store']);
-
-Route::put('sales/{sale}',['App\Http\Controllers\Api\V1\SalesController','update']);
-Route::put('sales/{id}/cancel', ['App\Http\Controllers\Api\V1\SalesController', 'cancel']);
-*/
-
 Route::get('products',['App\Http\Controllers\Api\V1\ProductsController','index']);
 Route::get('products/{product}',['App\Http\Controllers\Api\V1\ProductsController','show']);
 
